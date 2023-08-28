dc8a7f42aa1e0944b796194af9abb6f876b89531
rehjmov <nijatrahimov9@gmail.com>
2023-08-28 14:23:42 +0400

    some upd

--- src/main/java/az/coders/cityservice/Application.java
+++ src/main/java/az/coders/cityservice/Application.java
@@ -1,14 +1,9 @@
 package az.coders.cityservice;
-
-import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/az/coders/cityservice/service/CityService.java
+++ src/main/java/az/coders/cityservice/service/CityService.java
@@ -5,13 +5,13 @@ import az.coders.cityservice.model.City;
 import java.util.List;
 
 public interface CityService {
-    public List<City> getCities(String name);
+    List<City> getCities(String name);
 
-    public City createCity(City city);
+     City createCity(City city);
 
-    public void deleteCity(Long id);
+     void deleteCity(Long id);
 
-    public City getCityById(Long id);
+     City getCityById(Long id);
 
     void updateCity(City city, Long id);
 }
